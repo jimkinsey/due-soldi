b89a08741e6b7d565165e75098743950736ea27d
Jim Kinsey <jim.kinsey@springer.com>
2017-09-28 13:12:05 +0200

    Further simplify the index page maker

--- src/main/scala/duesoldi/page/IndexPageMaker.scala
+++ src/main/scala/duesoldi/page/IndexPageMaker.scala
@@ -6,6 +6,7 @@ import cats.data.EitherT
 import duesoldi.config.Config
 import duesoldi.markdown.MarkdownDocument
 import duesoldi.model.BlogEntry
+import duesoldi.page.IndexPageFailure.RenderFailure
 import duesoldi.rendering.{BlogIndexPageModel, Renderer}
 import duesoldi.storage.BlogStore
 import duesoldi.validation.ValidIdentifier
@@ -16,7 +17,6 @@ sealed trait IndexPageFailure
 object IndexPageFailure {
   case object BlogStoreEmpty extends IndexPageFailure
   case class RenderFailure(failure: Renderer.Failure) extends IndexPageFailure
-  case class UnexpectedFailure(failure: Any) extends IndexPageFailure
 }
 
 class IndexPageMaker(renderer: Renderer, blogStore: BlogStore, config: Config)(implicit executionContext: ExecutionContext) {
@@ -26,15 +26,16 @@ class IndexPageMaker(renderer: Renderer, blogStore: BlogStore, config: Config)(i
     (for {
       entries <- EitherT(blogEntries)
       model = pageModel(entries)
-      html <- EitherT(renderer.render("blog-index", model)).leftMap(_.asInstanceOf[Any])
+      html <- EitherT[Future, IndexPageFailure, String](renderPage(model))
     } yield {
       html
-    }).leftMap {
-      case IndexPageFailure.BlogStoreEmpty => IndexPageFailure.BlogStoreEmpty
-      case failure: Renderer.Failure => IndexPageFailure.RenderFailure(failure)
-      case failure => IndexPageFailure.UnexpectedFailure(failure)
-    }.value
+    }).value
   }
+
+  private def renderPage(model: BlogIndexPageModel): Future[Either[RenderFailure, String]] =
+    renderer
+      .render("blog-index", model)
+      .map(_.left.map(RenderFailure))
 
   private def blogEntries: Future[Either[IndexPageFailure.BlogStoreEmpty.type, Seq[BlogEntry]]] =
     blogStore.entries.map { entries =>
