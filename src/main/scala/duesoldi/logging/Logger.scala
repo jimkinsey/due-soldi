dda0f9d9b95e0773f80727aaaac409bb9e9d3c9f
jimkinsey <jim.kinsey@springer.com>
2017-10-04 23:33:38 +0200

    Include timestamp when logging

--- src/main/scala/duesoldi/logging/Logger.scala
+++ src/main/scala/duesoldi/logging/Logger.scala
@@ -1,17 +1,17 @@
 package duesoldi.logging
 
+import java.io.PrintStream
+import java.time.format.DateTimeFormatter
+import java.time.{ZoneOffset, ZonedDateTime}
+
 class Logger(name: String, loggingEnabled: Boolean = true) {
 
-  def info(message: => String) {
+  def info(message: => String) { log(message, System.out) }
+  def error(message: => String) { log(message, System.err) }
+
+  private def log(message: String, stream: PrintStream) = {
     if (loggingEnabled) {
-      System.out.println(s"[$name] $message")
+      stream.println(s"${ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ISO_DATE_TIME)} [$name] $message")
     }
   }
-
-  def error(message: => String) {
-    if (loggingEnabled) {
-      System.err.println(s"[$name] $message")
-    }
-  }
-
 }
