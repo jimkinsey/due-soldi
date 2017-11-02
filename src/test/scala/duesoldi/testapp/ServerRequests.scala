af7fa052ee7a6ff386eb04442e681555f7767dcf
jimkinsey <jim.kinsey@springer.com>
2017-11-02 23:33:16 +0100

    When testing, start only one app per run
     - big performance improvement in not starting an app for each request (down from ~10s to ~4s)
     - code is actually simpler as a result (and may be simplified further)

--- src/test/scala/duesoldi/testapp/ServerRequests.scala
+++ src/test/scala/duesoldi/testapp/ServerRequests.scala
@@ -9,15 +9,27 @@ import scala.concurrent.{ExecutionContext, Future}
 object ServerRequests {
 
   def get(path: String, headers: (String, String)*)(implicit ec: ExecutionContext, env: Env): Future[Response] = {
-    HttpClient.get(path, env, headers)
+    HttpClient.get(path, env, withOverrides(headers))
   }
 
   def put(path: String, body: String, headers: (String, String)*)(implicit executionContext: ExecutionContext, env: Env): Future[Response] = {
-    HttpClient.put(path, env, body, headers)
+    HttpClient.put(path, env, body, withOverrides(headers))
   }
 
   def delete(path: String, headers: (String, String)*)(implicit executionContext: ExecutionContext, env: Env): Future[Response] = {
-    HttpClient.delete(path, env, headers)
+    HttpClient.delete(path, env, withOverrides(headers))
+  }
+
+  private def withOverrides(headers: Seq[(String, String)])(implicit env: Env): Seq[(String, String)] = {
+    headers :+ headers.find(_._1 == "Config-Override").getOrElse(configOverrideHeader(env)) :+ secretKeyHeader(env)
+  }
+
+  private def configOverrideHeader(env: Env): (String, String) = {
+    "Config-Override" -> env.map { case (key, value) => s"$key=$value" } .toSeq.mkString(" ")
+  }
+
+  private def secretKeyHeader(env: Env): (String, String) = {
+    "Secret-Key" -> TestApp.secretKey
   }
 
 }
