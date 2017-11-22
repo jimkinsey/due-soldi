e6298ee7ad8a6a8e5955d06cebd657a06c25be46
Jim Kinsey <jim.kinsey@springer.com>
2017-11-22 11:50:00 +0100

    Stop using deprecated Dispatch API

--- src/test/scala/duesoldi/test/support/httpclient/HttpClient.scala
+++ src/test/scala/duesoldi/test/support/httpclient/HttpClient.scala
@@ -13,7 +13,7 @@ object HttpClient
   case class Response(status: Int, headers: Map[String, Seq[String]], body: String)
 
   def get(path: String, env: Env, headers: Seq[(String, String)] = Seq.empty)(implicit ec: ExecutionContext): Future[Response] = {
-    Http(url(s"http://localhost:${env("PORT")}$path").setHeaders(headers.map { case (name, value) => name -> Seq(value) } toMap)).map { res =>
+    Http.default(url(s"http://localhost:${env("PORT")}$path").setHeaders(headers.map { case (name, value) => name -> Seq(value) } toMap)).map { res =>
       Response(
         status = res.getStatusCode,
         headers = convertHeaders(res.getHeaders),
@@ -23,7 +23,7 @@ object HttpClient
   }
 
   def put(path: String, env: Env, body: String, headers: Seq[(String, String)] = Seq.empty)(implicit ec: ExecutionContext): Future[Response] = {
-    Http(url(s"http://localhost:${env("PORT")}$path").setMethod("PUT").setBody(body).setHeaders(headers.map { case (name, value) => name -> Seq(value) } toMap)).map { res =>
+    Http.default(url(s"http://localhost:${env("PORT")}$path").setMethod("PUT").setBody(body).setHeaders(headers.map { case (name, value) => name -> Seq(value) } toMap)).map { res =>
       Response(
         status = res.getStatusCode,
         headers = convertHeaders(res.getHeaders),
@@ -33,7 +33,7 @@ object HttpClient
   }
 
   def delete(path: String, env: Env, headers: Seq[(String, String)] = Seq.empty)(implicit ec: ExecutionContext): Future[Response] = {
-    Http(url(s"http://localhost:${env("PORT")}$path").setMethod("DELETE").setHeaders(headers.map { case (name, value) => name -> Seq(value) } toMap)).map { res =>
+    Http.default(url(s"http://localhost:${env("PORT")}$path").setMethod("DELETE").setHeaders(headers.map { case (name, value) => name -> Seq(value) } toMap)).map { res =>
       Response(
         status = res.getStatusCode,
         headers = convertHeaders(res.getHeaders),
