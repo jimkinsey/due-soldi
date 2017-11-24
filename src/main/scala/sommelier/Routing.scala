35a9d913c8a6e1ba6199e07350189de38812a3a4
Jim Kinsey <jim.kinsey@springer.com>
2017-11-24 13:22:20 +0100

    Early iteration of custom HTTP routing using the Sun-provided server

---
+++ src/main/scala/sommelier/Routing.scala
@@ -0,0 +1,14 @@
+package sommelier
+
+object Routing
+{
+  type Handler = (Request => Response)
+
+  lazy val GET = RequestMatcher(method = MethodMatcher(Method.GET))
+
+  implicit class RouteMaker(matcher: RequestMatcher) {
+    def respond(handler: Handler) = Route(matcher, handler)
+  }
+
+  implicit def statusToResponse(status: Int): Response = Response(status = status)
+}
---
+++ src/main/scala/sommelier/package.scala
@@ -0,0 +1,31 @@
+package object sommelier
+{
+  case class Request(method: Method, uri: String)
+
+  case class Response(status: Int, body: Option[String] = None)
+  {
+    def apply(body: String): Response = copy(body = Some(body))
+  }
+
+  case class Route(matcher: RequestMatcher, handle: Routing.Handler)
+
+  sealed trait Method
+  object Method
+  {
+    case object GET extends Method
+    case object HEAD extends Method
+    case object POST extends Method
+    case object PUT extends Method
+    case object DELETE extends Method
+
+    def apply(name: String): Method = {
+      name match {
+        case "GET" => GET
+        case "HEAD" => HEAD
+        case "POST" => POST
+        case "PUT" => PUT
+        case "DELETE" => DELETE
+      }
+    }
+  }
+}
