fab1a26c3713ca7d32bfafd1788b44d331a2d81a
jimkinsey <jim.kinsey@springer.com>
2017-11-29 22:20:25 +0100

    Sommelier: Improve the result handling code
     - adding recover removes some pattern matching boiler plate - the server doesn't care whether the result was async or not

--- src/main/scala/sommelier/Result.scala
+++ src/main/scala/sommelier/Result.scala
@@ -2,26 +2,36 @@ package sommelier
 
 import scala.concurrent.{ExecutionContext, Future}
 
-sealed trait Result[T]
+sealed trait Result[+T]
 {
   def flatMap[T1](fn: T => Result[T1]): Result[T1]
   def map[T1](fn: T => T1): Result[T1]
+  def recover[T1 >: T](fn: Rejection => T1): Result[T1]
 }
-sealed trait SyncResult[T] extends Result[T]
+
+sealed trait SyncResult[T]
+extends Result[T]
+
 object SyncResult
 {
-  case class Rejected[T](rejection: Rejection) extends SyncResult[T]
+  case class Rejected[T](rejection: Rejection)
+  extends SyncResult[T]
   {
     override def flatMap[T1](fn: (T) => Result[T1]): Result[T1] = Rejected(rejection)
     override def map[T1](fn: (T) => T1): Result[T1] = Rejected(rejection)
+    override def recover[T1](fn: (Rejection) => T1): Result[T1] = Accepted(fn(rejection))
   }
-  case class Accepted[T](result: T) extends SyncResult[T]
+  case class Accepted[T](result: T)
+  extends SyncResult[T]
   {
     override def flatMap[T1](fn: (T) => Result[T1]): Result[T1] = fn(result)
     override def map[T1](fn: (T) => T1): Result[T1] = Accepted(fn(result))
+    override def recover[T1 >: T](fn: Rejection => T1): Result[T1] = this
   }
 }
-case class AsyncResult[T](result: Future[Result[T]])(implicit executionContext: ExecutionContext) extends Result[T]
+
+case class AsyncResult[T](result: Future[Result[T]])(implicit executionContext: ExecutionContext)
+extends Result[T]
 {
   override def flatMap[T1](fn: (T) => Result[T1]): Result[T1] =
     AsyncResult(result.flatMap {
@@ -29,13 +39,14 @@ case class AsyncResult[T](result: Future[Result[T]])(implicit executionContext: 
       case SyncResult.Accepted(t) => Future.successful(fn(t))
       case AsyncResult(futT1) => futT1.map(_ flatMap fn)
     })
-  override def map[T1](fn: (T) => T1): Result[T1] =
-    AsyncResult(result.map(_.map(fn)))
+  override def map[T1](fn: (T) => T1): Result[T1] = AsyncResult(result.map(_ map fn))
+  override def recover[T1 >: T](fn: (Rejection) => T1) = AsyncResult(result.map(_ recover fn))
 }
+
 object Result
 {
-  implicit class OptionResult[T](opt: Option[T]) {
+  implicit class OptionResult[T](opt: Option[T])
+  {
     def rejectWith(ifNone: => Rejection): Result[T] = opt.fold[Result[T]](SyncResult.Rejected[T](ifNone))(SyncResult.Accepted[T])
-    def rejectWithResponse(ifNone: => Response): Result[T] = opt.fold[Result[T]](SyncResult.Rejected[T](Rejection(ifNone)))(SyncResult.Accepted[T])
   }
 }
