fd7bdc4ef297091ac6ce498e4b3b1e3e26b32b26
Jim Kinsey <jim.kinsey@springer.com>
2018-01-04 16:05:34 +0100

    Move the entry page making into the controller
     - this is easier now that the routing is simpler and it can all be done within the for comprehension in the route definition
     - means some events have been ditched, but they were arguably not very useful anyway

--- src/main/scala/duesoldi/blog/pages/package.scala
+++ src/main/scala/duesoldi/blog/pages/package.scala
@@ -4,7 +4,6 @@ import duesoldi.blog.model.BlogEntry
 
 package object pages
 {
-  type MakeEntryPage = (String) => EntryPageMaker.Result
   type MakeIndexPage = () => IndexPageMaker.Result
   type BuildEntryPageModel = (BlogEntry) => BlogEntryPageModel
   type BuildIndexPageModel = Seq[BlogEntry] => BlogIndexPageModel
