3426b600d7e6e7ba04307d44e8df576512129ece
Jim Kinsey <jim.kinsey@springer.com>
2018-04-03 17:30:27 +0200

    Load templates as resources from the assembled jar

--- core/src/duesoldi/rendering/Renderer.scala
+++ core/src/duesoldi/rendering/Renderer.scala
@@ -1,22 +1,25 @@
 package duesoldi.rendering
 
-import bhuj.MustacheBuilder.mustacheRenderer
+import java.io.{BufferedReader, InputStreamReader}
+
+import bhuj.Mustache
 import duesoldi.furniture.CurrentUrlPath
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Try
 
 object Renderer
 {
   import bhuj.context.ContextImplicits._
 
   def render(furniturePath: CurrentUrlPath)(implicit ec: ExecutionContext): Render = {
-    mustacheRenderer
-      .withHelpers(
-        "furniture" -> { (path, _) => Future.successful(furniturePath(path).map(_._1)) }
-      )
-      .withTemplatePath("core/resources/templates")
-      .withoutCache
-      .renderTemplate
+    new Mustache(getTemplate, Map("furniture" -> { (path: String, _: String => bhuj.Result) => Future.successful(furniturePath(path).map(_._1)) }) ).renderTemplate
   }
+
+  def getTemplate(name: String): Future[Option[String]] = Future.fromTry( Try {
+    val stream = this.getClass.getClassLoader.getResourceAsStream(s"templates/$name.mustache")
+    val reader = new BufferedReader(new InputStreamReader(stream))
+    Option(reader.lines().toArray.mkString("\n"))
+  })
 
 }
