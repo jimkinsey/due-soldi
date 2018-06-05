3b4f3bee9355c003f7995e3b4ff32149d36d7222
jimkinsey <jim.kinsey@springer.com>
2018-06-06 00:20:35 +0200

    Start to add basic cookie support to Sommelier

--- sommelier/src/sommelier/messaging/Cookie.scala
+++ sommelier/src/sommelier/messaging/Cookie.scala
@@ -3,3 +3,12 @@ package sommelier.messaging
 case class Cookie(name: String, value: String) {
   def format: String = s"$name=$value"
 }
+
+object Cookie
+{
+  def parse(string: String): Cookie = string match {
+    case Valid(name, value) => Cookie(name, value)
+  }
+
+  private val Valid = """^(\w+)=(.*)$""".r // FIXME broken
+}
--- sommelier/src/sommelier/messaging/Request.scala
+++ sommelier/src/sommelier/messaging/Request.scala
@@ -13,6 +13,10 @@ case class Request(
   def header(header: (String, Seq[String])): Request = copy(headers = headers + header)
   def body(str: String): Request = copy(body = Some(str))
   def host(host: String): Request = header("Host" -> Seq(host))
+  def cookies: Seq[Cookie] = headers.collect {
+    case (name, values) if name.toLowerCase == "cookie" => values.map(Cookie.parse)
+  }.flatten.toSeq
+  def cookie(name: String): Option[Cookie] = cookies.find(_.name == name)
 }
 
 
